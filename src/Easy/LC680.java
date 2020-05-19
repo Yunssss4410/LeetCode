2f5c1889ac907c736cb8550cfea77e2382ac47d4
Yunssss4410 <2419350961@qq.com>
2020-05-19 10:55:37 +0800

    680. 验证回文字符串 Ⅱ 状态:AC

---
+++ src/Easy/LC680.java
@@ -0,0 +1,30 @@
+package Easy;
+
+public class LC680 {
+    public static void main(String[] args) {
+        String s="abc";
+        System.out.println(new Solution().validPalindrome(s));
+    }
+    /*
+    680. 验证回文字符串 Ⅱ
+    URL：https://leetcode-cn.com/problems/valid-palindrome-ii/
+     */
+    static class Solution {
+        private boolean count=false;
+        public boolean validPalindrome(String s) {
+            int left=0,right=s.length()-1;
+            while (left<right)
+            {
+                if (s.charAt(left)==s.charAt(right)){
+                    left++;
+                    right--;
+                }else {
+                    if (count) return false;
+                    count=true;
+                    return validPalindrome(s.substring(left+1,right+1)) || validPalindrome(s.substring(left,right));
+                }
+            }
+            return true;
+        }
+    }
+}
