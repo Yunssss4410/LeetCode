ed8fc96623be91ab7302fe30d95029caac76c1fe
Yunssss4410 <2419350961@qq.com>
2020-05-22 15:12:11 +0800

    5. 最长回文子串 105. 从前序与中序遍历序列构造二叉树 1371. 每个元音包含偶数次的最长子字符串 状态:AC

---
+++ src/Middle/LC1371.java
@@ -0,0 +1,39 @@
+package Middle;
+
+import java.util.Arrays;
+
+public class LC1371 {
+    /*
+    1371. 每个元音包含偶数次的最长子字符串
+    URL:https://leetcode-cn.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
+     */
+    class Solution {
+        public int findTheLongestSubstring(String s) {
+            int n = s.length();
+            int[] pos = new int[1 << 5];
+            Arrays.fill(pos, -1);
+            int ans = 0, status = 0;
+            pos[0] = 0;
+            for (int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if (ch == 'a') {
+                    status ^= (1 << 0);
+                } else if (ch == 'e') {
+                    status ^= (1 << 1);
+                } else if (ch == 'i') {
+                    status ^= (1 << 2);
+                } else if (ch == 'o') {
+                    status ^= (1 << 3);
+                } else if (ch == 'u') {
+                    status ^= (1 << 4);
+                }
+                if (pos[status] >= 0) {
+                    ans = Math.max(ans, i + 1 - pos[status]);
+                } else {
+                    pos[status] = i + 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
